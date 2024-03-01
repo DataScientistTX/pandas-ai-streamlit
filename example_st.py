2e90d406f34fb65c9b6c771e05fdc5f9686b69c2
Sercan Gul <sercan.gul@gmail.com>
2024-03-01 00:14:05 -0600

    trials

---
+++ example_st.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from pandasai import Agent
+from pandasai.llm import OpenAI
+from pandasai.responses.streamlit_response import StreamlitResponse
+
+employees_df = pd.DataFrame(
+    {
+        "EmployeeID": [1, 2, 3, 4, 5],
+        "Name": ["John", "Emma", "Liam", "Olivia", "William"],
+        "Department": ["HR", "Sales", "IT", "Marketing", "Finance"],
+    }
+)
+
+salaries_df = pd.DataFrame(
+    {
+        "EmployeeID": [1, 2, 3, 4, 5],
+        "Salary": [5000, 6000, 4500, 7000, 5500],
+    }
+)
+
+llm = OpenAI()
+dl = Agent(
+    [employees_df, salaries_df],
+    config={"llm": llm, "verbose": True, "response_parser": StreamlitResponse},
+)
+
+dl.chat("Plot salaries against employee name")
